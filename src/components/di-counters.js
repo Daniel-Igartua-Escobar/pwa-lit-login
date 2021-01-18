9694875fad4ed1ae2bebeea9833ee0e471ab4f57
daniel.igartua <daniel.igartua@unidadeditorial.es>
2021-01-18 16:09:36 +0100

    add counter component

---
+++ src/components/di-counters.js
@@ -0,0 +1,79 @@
+import { LitElement, html, css } from 'lit-element';
+
+
+export class DiCounters extends LitElement {
+  static get properties() {
+    return {
+      counters: {type: Array}
+    };
+  }
+
+  constructor() {
+    super();
+    this.counters = [
+      {
+        name: 'days',
+        number: '00'
+      },
+      {
+        name: 'hours',
+        number: '00'
+      },
+      {
+        name: 'minutes',
+        number: '00'
+      },
+      {
+        name: 'seconds',
+        number: '00'
+      }
+    ]
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+
+      .counters {
+        display: flex;
+        justify-content: center;
+        color: gray;
+      }
+
+      .count {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+
+      .count:not(:last-child) {
+        margin-right: 1.5rem;
+      }
+
+      .number {
+        font-size: 5rem;
+      }
+
+      .name {
+        font-size: 1.5rem;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <div class="counters">
+        ${this.counters.map(counter => html`
+          <span class="count">
+            <span class="number">${counter.number}</span>
+            <span class="name">${counter.name}</span>
+          </span>
+        `)}
+      </div>
+    `;
+  }
+}
+
+window.customElements.define('di-counters', DiCounters);
